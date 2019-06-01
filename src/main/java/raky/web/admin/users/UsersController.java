0747efeb6b541cfce55e52f2863782f9bfadd33e
hcl <123456>
2019-06-01 15:43:17 +0800

    add 控制器基本增删改查

--- src/main/java/raky/web/admin/users/UsersController.java
+++ src/main/java/raky/web/admin/users/UsersController.java
@@ -3,18 +3,16 @@ package raky.web.admin.users;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 import raky.entity.Users;
 import raky.service.UsersService;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/users")
 public class UsersController {
 
@@ -34,29 +32,24 @@ public class UsersController {
 
     }
 
-    @RequestMapping(value = "/delete/{id}",method = RequestMethod.GET)
-    public String delete(@PathVariable("id") Long id){
+    @RequestMapping(value = "/delete",method = RequestMethod.GET)
+    public String delete(Long id){
         usersService.delete(id);
-        return "redirect:";
+        return "redirect:/list";
     }
 
     @RequestMapping(value = "/input",method = RequestMethod.GET)
-    public ModelAndView getOne(Long id, ModelMap model){
-        model.put("user",usersService.getOne(id));
-        return new ModelAndView("",model);
+    public String getOne(Long id, ModelMap model){
+        logger.info("xxx");
+        model.addAttribute("user",usersService.getOne(id));
+        return "/index";
     }
 
     @RequestMapping(value = "/list",method = RequestMethod.GET)
-    public ModelAndView getList(ModelMap model){
+    public String getList(ModelMap model){
         List<Users> usersList = usersService.getList(new Users());
-        return new ModelAndView("",model);
+        model.addAttribute("usersList",usersList);
+        return "/users/list";
     }
 
-
-//    @RequestMapping(value = "/update/{id}",method = RequestMethod.POST) //RequestMethod.PUT
-//    public String update(@PathVariable("id")Long id , @RequestParam(value = "user_name",required = true)String user_name,
-//                         @RequestParam(value = "pass_word" ,required = true)String pass_word){
-//
-//        return null;
-//    }
 }
