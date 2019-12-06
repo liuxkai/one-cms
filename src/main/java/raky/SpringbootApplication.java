11e9cc55be75d26ade819fda9682f5b10807687b
Mihich <1518477995@qq.com>
2019-12-06 11:43:52 +0800

    第6次结

--- src/main/java/raky/SpringbootApplication.java
+++ src/main/java/raky/SpringbootApplication.java
@@ -2,13 +2,13 @@ package raky;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+@Configuration
 @SpringBootApplication
 @MapperScan("raky.dao")
 public class SpringbootApplication implements WebMvcConfigurer {
@@ -16,6 +16,7 @@ public class SpringbootApplication implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/image/**").addResourceLocations("file:D:/src/");
         WebMvcConfigurer.super.addResourceHandlers(registry);
     }
 
