8df5ef327a231fefdd916f4352f1d7908d9874e5
liuxkai <1137208365@qq.com>
2019-12-11 15:00:37 +0800

    12011第八次提交

--- src/main/java/raky/SpringbootApplication.java
+++ src/main/java/raky/SpringbootApplication.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringbootApplication implements WebMvcConfigurer {
 
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
